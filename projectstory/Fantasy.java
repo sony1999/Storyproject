7f5d083f98cc567e64c14e856423f9dd231769f6
sonysai <55012080+sony1999@users.noreply.github.com>
2021-05-06 12:13:35 +0530

    Update Fantasy.java

--- projectstory/Fantasy.java
+++ projectstory/Fantasy.java
@@ -8,6 +8,9 @@ public class Fantasy extends Story implements IGenre{
 	Fantasy(String name, String author, int chapters,int rating,int views) {
 		super(name, author, chapters,rating,views);
 	}
+	/*
+	* check_chapters(): checks the no of chapters in the fantasy are less than 20
+	*/
 	public boolean check_chapters(int chapter) {
 		if(chapter<20) {
 			return true;
