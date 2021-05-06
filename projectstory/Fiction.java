58974711fa863e9b83e44ba56a2b72ba4e2e0de2
sonysai <55012080+sony1999@users.noreply.github.com>
2021-05-06 12:14:25 +0530

    Update Fiction.java

--- projectstory/Fiction.java
+++ projectstory/Fiction.java
@@ -8,6 +8,9 @@ public class Fiction extends Story implements IGenre{
 	Fiction(String name, String author, int chapters,int rating,int views) {
 		super(name,author,chapters,rating,views);
 	}
+	/*
+	* check_chapters(): checks the no of chapters in the fiction are greater than 20 and less than 30
+	*/
 	public boolean check_chapters(int chapter) {
 		if(chapter<30 && chapter >20) {
 			return true;
