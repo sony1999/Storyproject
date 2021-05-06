ed8d485c213d7b2acd5615dbf24d2a1eb86515c5
sonysai <55012080+sony1999@users.noreply.github.com>
2021-05-06 12:15:30 +0530

    Update IGenre.java

--- projectstory/IGenre.java
+++ projectstory/IGenre.java
@@ -1,5 +1,8 @@
 package projectstory;
 
 public interface IGenre {
+	/*
+	* check_chapters(): checks the no of chapters in the genre and returns boolean value
+	*/
 	public boolean check_chapters(int chapter) ;
 }
